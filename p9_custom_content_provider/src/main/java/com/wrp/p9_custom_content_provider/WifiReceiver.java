0bdfc9cc65b2e6a13283505f2cde4e1abd889f5b
girishramnani <girishramnani95@gmail.com>
2016-04-21 12:27:59 +0530

    stub added

---
+++ p9_custom_content_provider/src/main/java/com/wrp/p9_custom_content_provider/WifiReceiver.java
@@ -0,0 +1,15 @@
+package com.wrp.p9_custom_content_provider;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WifiReceiver extends BroadcastReceiver {
+    public WifiReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
