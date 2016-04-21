2727bca99be67b8c2a52abd49f2bc56399cfc800
girishramnani <girishramnani95@gmail.com>
2016-04-21 11:45:11 +0530

    changed the database module and added stub

---
+++ p5_database_custom_broadcast/src/main/java/com/wrp/p5_database/DatabaseReceiver.java
@@ -0,0 +1,17 @@
+package com.wrp.p5_database;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DatabaseReceiver extends BroadcastReceiver {
+    public DatabaseReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
